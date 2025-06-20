5c08a084ce54c47585eecc718fcd26dadeff635c
The-Duminy <johnpaul_chamings@yahoo.com.au>
2025-06-20 11:38:06 +0000

    login redirect first test

--- part2/app.js
+++ part2/app.js
@@ -3,10 +3,19 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
+const session = require('express-session'); //Q13 Edit
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+//Q13 Edits Begin here
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
+//Q13 Edits End here
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,23 +36,32 @@ router.get('/me', (req, res) => {
 });
 
 // POST login (dummy version)
+// Q13 Edits Begin Here
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { user, pass } = req.body;
 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
-    `, [email, password]);
+      WHERE username = ? AND password_hash = ?
+    `, [user, pass]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    res.json({ message: 'Login successful', user: rows[0] });
+    req.session.user = {
+      user_id: rows[0].user_id,
+      username: rows[0].username,
+      role: rows[0].role
+    };
+
+    res.json({ message: 'Login successful', role: rows[0].role });
+
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
 });
+// Q13 Edits End Here
 
 module.exports = router;
